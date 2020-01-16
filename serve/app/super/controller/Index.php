ae054389734094ddfaca089ccfd9ac05be5a4ee6
ken <ken@qq.com>
2020-01-16 13:04:43 +0800

    项目框架

---
+++ serve/app/super/controller/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+
+namespace app\super\controller;
+
+
+class Index
+{
+    public function index()
+    {
+        return 'super-index';
+    }
+}
---
+++ serve/runtime/index/temp/d81e58466cb0af8e71134c804fd570bc.php
@@ -0,0 +1,16 @@
+<?php /*a:1:{s:61:"E:\php\PHPTutorial\WWW\science\app\index\view\index\index.vue";i:1578992901;}*/ ?>
+<template>
+    <view id="app">
+        <view>
+            hello
+        </view>
+    </view>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
